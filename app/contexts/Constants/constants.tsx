b0e4d39fe3cc949f0cb1394f00d50ec2c06582e0
Ho Tan Hung <tanhungho2002@gmail.com>
2025-06-09 12:53:23 +0700

    feat: update production API URL in constants file

--- app/contexts/Constants/constants.tsx
+++ app/contexts/Constants/constants.tsx
@@ -1,6 +1,6 @@
 export const apiURL =
   process.env.NODE_ENV === "production"
-    ? "http://localhost:5000/api" // Cập nhật đúng URL API thật
+    ? "backend-booked-movie-ticket.onrender.com/api" // Cập nhật đúng URL API thật
     : "http://localhost:5000/api"; // Nếu API chạy local
 
 export const LOCAL_STORAGE_TOKEN_NAME = "token-user";
