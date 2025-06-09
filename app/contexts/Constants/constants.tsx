bfbc109e6fba88cc60b2f82b36ae55b8f67e5317
Ho Tan Hung <tanhungho2002@gmail.com>
2025-06-09 13:00:10 +0700

    feat: fix production API URL by adding HTTPS protocol

--- app/contexts/Constants/constants.tsx
+++ app/contexts/Constants/constants.tsx
@@ -1,6 +1,6 @@
 export const apiURL =
   process.env.NODE_ENV === "production"
-    ? "backend-booked-movie-ticket.onrender.com/api" // Cập nhật đúng URL API thật
+    ? "https://backend-booked-movie-ticket.onrender.com/api" // Cập nhật đúng URL API thật
     : "http://localhost:5000/api"; // Nếu API chạy local
 
 export const LOCAL_STORAGE_TOKEN_NAME = "token-user";
